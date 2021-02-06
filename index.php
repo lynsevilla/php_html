77144b844376603fcf933a8f33ac2e99cb8a1041
Carelyn Sevilla <48046065+lynsevilla@users.noreply.github.com>
2021-02-06 15:35:12 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    </head>
+<body>
+
+<h1> Hello! </h1>
+
+<?php include 'alert.php'
+?>
+
+<?php
+$myName = "Carelyn";
+$firstThing = "that it can be quite exciting";
+$secondThing = "it's challenging";
+$thirdThing = "I learn new stuff everyday.";
+echo "My name is $myName and the top things I like about coding is $firstThing, $secondThing and $thirdThing";
+
+?>    
+
+</body>    
+</html>
